da441b46b4d84724b896d9f8b8d62957517a2df6
Gabriel Pittol <gabrielpittol@hotmail.com>
2021-10-31 17:44:35 -0300

    Menu & Minimap Improvements

--- Assets/Scripts/Water.cs
+++ Assets/Scripts/Water.cs
@@ -7,9 +7,22 @@ public class Water : MonoBehaviour
 {
     public float speed;
 
+    public bool moveBoth = false;
+    public bool invert = true;
+
     private void Update()
     {
-        transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y,
+        if(!moveBoth) transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y,
             transform.position.z);
+        else
+        {
+            if(!invert) transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y + speed * Time.deltaTime,
+                transform.position.z);
+            else
+            {
+                transform.position = new Vector3(transform.position.x + speed * Time.deltaTime, transform.position.y - speed * Time.deltaTime,
+                    transform.position.z);
+            }
+        }
     }
 }
